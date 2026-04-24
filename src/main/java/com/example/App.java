8f28c3159582e4446453eeb4082dbbfe50debc21
Vishnudev s v <vishnudevsv224488@gmail.com>
2026-04-24 12:42:21 +0530

    Updated App.java and Docker run output (24-04-2026)

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,6 +3,6 @@ package com.example;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello from Docker + Maven Integration!");
-        Thread.sleep(600000); // keep container running
+        Thread.sleep(600000);
     }
 }
