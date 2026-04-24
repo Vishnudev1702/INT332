1d6df914a2e6cd9aa2e0ab8f17ceaac75b6b5f24
Vishnudev s v <vishnudevsv224488@gmail.com>
2026-04-24 12:38:23 +0530

    Docker + Maven container running successfully (24-04-2026)

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,8 @@
 package com.example;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello from Docker + Maven Integration!");
+        Thread.sleep(600000); // keep container running
     }
 }
